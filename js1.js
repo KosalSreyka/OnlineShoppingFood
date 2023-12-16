0bea5c8f95b5f142e193420b454809d4bed20829
Sreyka Kosal <opo235664@gmail.com>
2023-12-16 22:21:27 +0700

    first commit

---
+++ js1.js
@@ -0,0 +1,20 @@
+function changeImg(sr)
+{
+        document.getElementById('img').src=sr;
+        
+       
+}
+function changeImg1(sr)
+{
+          document.getElementById('img1').src=sr;
+}
+function changeImg2(sr)
+{
+        document.getElementById('img2').src=sr;
+        
+       
+}
+function changeImg3(sr)
+{
+          document.getElementById('img3').src=sr;
+}
